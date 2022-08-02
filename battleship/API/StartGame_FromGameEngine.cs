b1e136fc6481a9d6c710128bb6b594c3b808c8e9
richard-shepherd <richard.s.shepherd@gmail.com>
2022-08-02 13:49:18 +0100

    Creating the code API

---
+++ battleship/API/StartGame_FromGameEngine.cs
@@ -0,0 +1,26 @@
+﻿namespace API
+{
+    /// <summary>
+    /// Code representation of the JSON message sent from the game engine
+    /// to each AI at the start of a new game.
+    /// </summary>
+    public class StartGame_FromGameEngine
+    {
+        /// <summary>
+        /// Gets or sets the event name.
+        /// </summary>
+        public string EventName { get; set; } = "START_GAME";
+
+        /// <summary>
+        /// Gets or sets the size of the board in the x-axis.
+        /// </summary>
+        public int BoardSize_X { get; set; } = 100;
+
+        /// <summary>
+        /// Gets or sets the size of the board in the y-axis.
+        /// </summary>
+        public int BoardSize_Y { get; set; } = 100;
+
+
+    }
+}
---
+++ battleship/Utility/Utils.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Utility
+{
+    /// <summary>
+    /// Static utility functions.
+    /// </summary>
+    public class Utils
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Converts the object passed in to a JSON string.
+        /// </summary>
+        public static string toJSON(object o)
+        {
+            if(o == null)
+            {
+                return "";
+            }
+            var settings = getJSONSerializerSettings();
+            return JsonConvert.SerializeObject(o, Formatting.None, settings);
+        }
+
+        /// <summary>
+        /// Returns an object of type T deserialized from the JSON string passed in.
+        /// </summary>
+        public static T fromJSON<T>(string json)
+        {
+            var settings = getJSONSerializerSettings();
+            var deserializedObject = JsonConvert.DeserializeObject<T>(json, settings);
+            if(deserializedObject == null)
+            {
+                throw new Exception($"Failed to deserialize {json} as type {typeof(T).Name}");
+            }
+            return deserializedObject;
+        }
+
+        #endregion
+
+        #region Private functions
+
+        /// <summary>
+        /// Returns the settings to use when serializing objects to and from JSON.
+        /// </summary>
+        private static JsonSerializerSettings getJSONSerializerSettings()
+        {
+            if(m_jsonSerializerSettings == null)
+            {
+                m_jsonSerializerSettings = new JsonSerializerSettings();
+                m_jsonSerializerSettings.Converters.Add(new StringEnumConverter());
+            }
+            return m_jsonSerializerSettings;
+        }
+
+        #endregion
+
+        #region Private data
+
+        // Controls how JSON is serialized and deserialized...
+        private static JsonSerializerSettings m_jsonSerializerSettings = null;
+
+        #endregion
+    }
+}
